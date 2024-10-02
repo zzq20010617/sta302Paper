503aee2662d4c96165bcffa4243c8cdadcefd51a
Ziqi Zhu <zzq20010617@gmail.com>
2024-10-02 09:39:01 -0400

    set up initial files

---
+++ scripts/00-download_data.R
@@ -0,0 +1,18 @@
+#### Preamble ####
+# Purpose: Downloads and saves the data from internet
+# Author: Ziqi Zhu
+# Date: 2 October 2024
+# Contact: ziqi.zhu@mail.utoronto.ca 
+# License: MIT
+# Pre-requisites: None
+
+
+#### Workspace setup ####
+library(readr)
+
+
+
+#### Save data ####
+write_csv(data, "data/raw_data/raw_data.csv") 
+
+         
---
+++ scripts/02-preliminary_data.R
@@ -0,0 +1,11 @@
+#### Preamble ####
+# Purpose: 
+# Author: Ziqi Zhu
+# Date: 2 October 2024
+# Contact: ziqi.zhu@mail.utoronto.ca 
+# License: MIT
+# Pre-requisites: None
+
+#### Workspace setup ####
+
+#### preliminary data ####
